14efae5b5ea5b65a0483ad1e419aa82c5834997e
Humblejoshua <humblejoshua52@gmail.com>
2024-11-13 10:45:23 +0300

    Add files via upload

---
+++ objectUtils.js
@@ -0,0 +1,8 @@
+// objectUtils.js
+export function mergeObjects(obj1, obj2) {
+    return { ...obj1, ...obj2 };
+}
+
+export function cloneObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
